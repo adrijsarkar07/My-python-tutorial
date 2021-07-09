cecac79770c2d8781436570754bda454bca184f5
Adrij <adrijsarkar9008@gmail.com>
2021-07-09 22:03:54 +0530

    I have code armstrong numbers here

---
+++ armstrong_no.py
@@ -0,0 +1,23 @@
+'''for i in range(1001):
+    num = i
+    result = 0
+    n =len(str(i))
+    while(i!=0):
+        digit = i%10
+        result=result+digit**n
+        i = i//10
+    if num==result:
+        print(num)'''
+n1 = int(input("Enter number"))
+n2 = int(input("Enter number"))
+
+for i in range(n1,n2):
+    num = i
+    result = 0
+    n =len(str(i))
+    while(i!=0):
+        digit = i%10
+        result=result+digit**n
+        i = i//10
+    if num==result:
+        print(num)
